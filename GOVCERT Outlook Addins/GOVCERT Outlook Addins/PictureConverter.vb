6c24fe660102e5bf7ef4b8517875842d55d423f4
Jean-Paul Weber <jean-paul.weber@govcert.etat.lu>
2019-01-18 14:19:06 +0100

    initial

---
+++ GOVCERT Outlook Addins/GOVCERT Outlook Addins/PictureConverter.vb
@@ -0,0 +1,26 @@
+﻿' Source: https://docs.microsoft.com/en-us/previous-versions/office/developer/office-2007/aa338202(v=office.12)'
+
+Imports System.Windows.Forms
+
+'Prevention from opening with the designer'
+<System.ComponentModel.DesignerCategory("")>
+Public Class PictureConverter
+    Inherits AxHost
+
+    Private Sub New()
+        MyBase.New(String.Empty)
+    End Sub
+
+    Public Shared Function ImageToPictureDisp(ByVal image As System.Drawing.Image) As stdole.IPictureDisp
+        Return CType(GetIPictureDispFromPicture(image), stdole.IPictureDisp)
+    End Function
+
+    Public Shared Function IconToPictureDisp(ByVal icon As System.Drawing.Icon) As stdole.IPictureDisp
+        Return ImageToPictureDisp(icon.ToBitmap())
+    End Function
+
+    Public Shared Function PictureDispToImage(ByVal picture As stdole.IPictureDisp) As System.Drawing.Image
+        Return GetPictureFromIPicture(picture)
+    End Function
+
+End Class
---
+++ GOVCERT Outlook Addins/GOVCERT Outlook Addins/ThisAddIn.vb
@@ -0,0 +1,42 @@
+﻿' Copyright (C) 2018, CERT Gouvernemental (GOVCERT.LU) '
+' Author: Jean-Paul Weber <jean-paul.weber@govcertt.etat.lu> '
+
+Public Class ThisAddIn
+
+    Private Sub ThisAddIn_Startup() Handles Me.Startup
+        ' Remove Keys for office 2016 '
+        RemoveLoadingTimesKeys("16.0")
+        ' Remove Keys for office 2013 '
+        RemoveLoadingTimesKeys("15.0")
+        ' Remove Keys for office 2010 '
+        RemoveLoadingTimesKeys("14.0")
+
+    End Sub
+
+    Private Sub RemoveLoadingTimesKeys(outlookVersion As String)
+        ' Remove loading times -> Workaround '
+        Dim outlookBase As Microsoft.Win32.RegistryKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Office\" + outlookVersion + "\Outlook", True)
+        ' If the key does not exit then the returned value is Nothing '
+        If Not outlookBase Is Nothing Then
+            Dim addinsKey As Microsoft.Win32.RegistryKey = outlookBase.OpenSubKey("Addins\Govcert Outlook Addins", True)
+            If Not addinsKey Is Nothing Then
+                For Each valueKey As String In addinsKey.GetValueNames
+                    addinsKey.DeleteValue(valueKey)
+                Next
+                addinsKey.Close()
+            End If
+            ' Note: Under AddInLoadTimes are also stored loading times however they don't provoke the disabling as they store only the 3 last loading times :/  '
+            outlookBase.Close()
+        End If
+
+    End Sub
+
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+    Protected Overrides Function CreateRibbonExtensibilityObject() As Microsoft.Office.Core.IRibbonExtensibility
+        Return New GOVCERTOutlookRibbon()
+    End Function
+
+End Class
